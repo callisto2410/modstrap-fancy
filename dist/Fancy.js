1bb025bf99246a9b42039b73463c303eed073cc2
Callisto <callisto2410@yandex.ru>
2020-12-15 16:23:31 +0900

    Rebuild

--- dist/Fancy.js
+++ dist/Fancy.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const lexicon_1 = require("@modstrap/lexicon");
+const lexicon_1 = __importDefault(require("@modstrap/lexicon"));
 require("@fancyapps/fancybox");
 /**
  * Adaptation for jQuery.FancyBox.
